27ccdf7a8e4870c380845efae51a1a4b54b91bf7
Mateusz <146337415+Crisper98@users.noreply.github.com>
2023-10-05 13:52:34 +0200

    Add files via upload

---
+++ 05.10.2023/about-us.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <title>HomePage</title>
+    <link 
+</head>
+<body>
+    <nav>
+        <ul>
+            <li><a href="index.php">HomePage</a></li>
+            <li><a href="about-us.php">About us</a></li>
+            <li><a href="contact.php">Contact</a></li>
+        </ul>
+    </nav>
+    <header><h1>O nas</h1></header>
+    <p>Ej, cały czas błądzę, ale staram się szukać wyjścia (a) Wczoraj nieistotne, bo to dzisiaj jakby wykorzystać (a)</p>
+    <p>Ej, przyniosę wreszcie im CV jak kwit Ej, mam dwie prawe ręce, jak Billy the Kid</p>
+    <footer>
+        Copyright &copy; <?php echo date("Y"); ?>
+    </footer>
+</body>
+</html>
---
+++ 05.10.2023/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <title>HomePage</title>
+</head>
+<body>
+    <nav>
+        <ul>
+            <li><a href="index.php">HomePage</a></li>
+            <li><a href="about-us.php">About us</a></li>
+            <li><a href="contact.php">Contact</a></li>
+        </ul>
+    </nav>
+    <header>
+        <h1>Strona główna</h1>
+    </header>
+        <p>Have you got colour in your cheeks? Do you ever get that fear that you can't shift</p>
+    <footer>
+        Copyright &copy; <?php echo date("Y"); ?>
+    </footer>
+</body>
+</html>
